5359c0bbb57def089c1aabe2bdd696c1e167ff04
Ondrej Platek <ondrej.platek@seznam.cz>
2015-01-15 00:54:32 +0100

    add logging

--- app/cleverbot/run.py
+++ app/cleverbot/run.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 from flask import Flask, render_template
 import argparse
+import logging
 
 app = Flask(__name__)
 app.secret_key = 12345  # TODO
@@ -19,6 +20,13 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--debug', dest='debug', action='store_true')
     parser.add_argument('--no-debug', dest='debug', action='store_false')
     parser.set_defaults(debug=True)
+    parser.add_argument('-l', '--log', default='cleverbot.log')
     args= parser.parse_args()
+
+    file_handler = logging.FileHandler(args.log, encoding='utf8')
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s [%(pathname)s:%(lineno)d]: \n%(message)s '))
+    file_handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(file_handler)
 
     app.run(host=args.host, port=args.port, debug=args.debug)
